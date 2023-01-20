429999b5c6414b778ef73f73069953e07a9524d1
Gagandeep Singh <gaggi.148@gmail.com>
2023-01-21 00:39:15 +0530

    Modified App.js, LibraryCOntainer, SongComp jsx and cs

--- src/Components/LibraryContainer/LibraryContainer.jsx
+++ src/Components/LibraryContainer/LibraryContainer.jsx
@@ -1,18 +1,40 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './LibraryContainer.module.css'
 import { MdArrowForwardIos } from 'react-icons/md'
 import SongComp from '../SongComp/SongComp'
 
 function LibraryContainer() {
+  const data = [
+    {index: 1, songName: "DilWale" },
+    {index: 2, songName: "Sanam Re" },
+    {index: 3, songName: "Apna Bana Le" },
+    {index: 4, songName: "Apna Bana De" },
+    {index: 5, songName: "Apna Bana De" },
+    {index: 6, songName: "Apna Bana De" },
+  ]
+  
+  // For scrolling the Library Container by clicking on the Arrow button
+  const libContRef = useRef();
+  const scrollFunc = (scrollVal) => {
+    libContRef.current.scrollLeft += scrollVal
+  }
+
   return (
+    <div className={styles.outerLibCont}>
     <div
+        ref={libContRef}
         style={{
             backgroundImage: ``
         }}
         className={styles.libCont}>
-        <SongComp/>
-        <span className={`${styles.arrCont} ${styles.right}`}><MdArrowForwardIos/></span>
-        <span className={`${styles.arrCont} ${styles.left}`}><MdArrowForwardIos/></span>
+        {data.map((items)=><SongComp
+          key={items.index}
+          songNameProp={items.songName}
+          imageUrl=""
+        />)}
+    </div>
+    <span onClick={()=>scrollFunc(-500)} className={`${styles.arrCont} ${styles.left}`}><MdArrowForwardIos/></span>
+    <span onClick={()=>scrollFunc(500)} className={`${styles.arrCont} ${styles.right}`}><MdArrowForwardIos/></span>
     </div>
   )
 }
--- src/Components/SongComp/SongComp.jsx
+++ src/Components/SongComp/SongComp.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import styles from './SongComp.module.css'
 import { BsFillPlayCircleFill } from 'react-icons/bs'
 
-function SongComp() {
+function SongComp(props) {
   return (
     <div
         style={{
-            backgroundImage: ``
+            backgroundImage: `${props.imageUrl}`
         }}
-        className={styles.songCont}>SongComp
+        className={styles.songCont}>
         <span className={styles.playBtn}><BsFillPlayCircleFill/></span>
+        <span className={styles.songName}>{props.songNameProp}</span>
     </div>
   )
 }
