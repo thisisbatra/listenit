1ffae57b3cca8f2d5712c5d458524b4767c1a597
thisisbatra <batranitin118@gmail.com>
2023-01-26 21:47:03 +0530

    some changes in libraryContainer

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Header.module.css';
 import {AiFillHome, AiOutlineSearch} from 'react-icons/ai';
 import {FaUserCircle} from 'react-icons/fa';
@@ -7,7 +7,12 @@ import {BiMenu} from 'react-icons/bi';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+    const [inputVal,setInputVal]=useState();
     const inputPlaceholder="enter artist, album, song name to get results"
+    const handler=(e)=>{
+        setInputVal(e.target.value);
+    }
+    // console.log("from state",inputVal);
     return (
         <nav className={styles.nav}>
             <ul>
@@ -17,7 +22,7 @@ const Header = () => {
                 </span>
                 <div className={styles.input}>
                 <li><AiOutlineSearch/></li>
-                <input type="text" name="input" placeholder={inputPlaceholder}/>
+                <input type="text" name="input" placeholder={inputPlaceholder} onChange={handler}/>
                 </div>
                 <span>
                 <li><BsSuitHeart/></li>
