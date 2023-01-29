541c19f855b736331342c1ea5719149ac5388618
Gagandeep Singh <gaggi.148@gmail.com>
2023-01-30 00:18:37 +0530

    Modified Header css, SongPlayer jsx and css

--- src/Components/SongPlayer/SongPlayer.jsx
+++ src/Components/SongPlayer/SongPlayer.jsx
@@ -1,23 +1,60 @@
-import React, { useContext } from 'react'
-import SongContext from '../../Context/SongContext'
-import styles from './SongPlayer.module.css'
+import React, { useContext, useState } from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import SongContext from "../../Context/SongContext";
+import styles from "./SongPlayer.module.css";
 // import { BsFillPlayCircleFill } from 'react-icons/bs'
 
 function SongPlayer(props) {
+  const [like, setLike] = useState(false);
+
+  const [songForSongPlayer] = useContext(SongContext);
+  let songName = songForSongPlayer.trackName;
+  let artist = songForSongPlayer.artistName;
+  let movie = songForSongPlayer.collectionName;
+  let imgSrc =
+    "https://play-lh.googleusercontent.com/IeNJWoKYx1waOhfWF6TiuSiWBLfqLb18lmZYXSgsH1fvb8v1IYiZr5aYWe0Gxu-pVZX3=w240-h480-rw";
   
-  const [songForSongPlayer]=useContext(SongContext);
-  // console.log(songForSongPlayer.url)
+  // const toggleLike = () => {
+  //   setLike((prev)=>!prev);
+  // }
+
   return (
     <div className={styles.songPlayerCont}>
-        SongPlayer
-        <div className={styles.controlsCont}>
-          <audio src={songForSongPlayer.url||songForSongPlayer.previewUrl} controls autoPlay>
-              audio not supported:(
-          </audio>
-          <div className={styles.songNameCont}>{songForSongPlayer.trackName}</div>
+      <div className={styles.controlsCont}>
+        
+        <div className={styles.songName_HeartCont}>
+          <div className={styles.songNameCont}>
+            <span className={styles.songNameClass}>
+              {songName ? songName : "No Song Playing"}
+            </span>
+            <span className={styles.artist_n_Movie} style={{ width: "100%", display: "flex" }}>
+              <span className={styles.artist}>
+                {artist ? artist : "Artist Name"}
+              </span>
+              &nbsp;•&nbsp;
+              <span className={styles.movie}>
+                {movie ? movie : "Movie Name"}
+              </span>
+            </span>
+          </div>
+          <span className={styles.heart} onClick={()=>setLike((prev)=>!prev)}>
+            {like? <AiFillHeart/>
+            :<AiOutlineHeart/>}
+          </span>
         </div>
+        
+        <audio
+          className={styles.audioTag}
+          src={songForSongPlayer.url || songForSongPlayer.previewUrl}
+          controls
+          autoPlay
+        >
+          audio not supported:(
+        </audio>
+        <img className={styles.imgTag} src={imgSrc} alt="Song Thumbnail" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default SongPlayer
+export default SongPlayer;
