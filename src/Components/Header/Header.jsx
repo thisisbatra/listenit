82ba017a8abc96008dca496e2c2bff49bbe1798c
Gagandeep Singh <gaggi.148@gmail.com>
2023-02-10 16:04:54 +0530

    Modified Header jsx and css, Like.jsx and index.css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,49 +1,75 @@
-import React, { useState } from 'react';
-import styles from './Header.module.css';
-import {AiFillHome, AiOutlineSearch} from 'react-icons/ai';
-// import {FaUserCircle} from 'react-icons/fa';
-// import {BsSuitHeart} from 'react-icons/bs';
-import {BiMenu} from 'react-icons/bi';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import styles from "./Header.module.css";
+import { AiOutlineSearch } from "react-icons/ai";
+import { RiHome2Fill, RiHome2Line } from "react-icons/ri";
+import { BiMenu } from "react-icons/bi";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
-    const [inputVal,setInputVal]=useState();
-    const inputPlaceholder="enter artist, album, song name to get results"
-    const navigate=useNavigate();
+  const [inputVal, setInputVal] = useState();
+  const inputPlaceholder = "Enter artist, album, song name to get results";
+  const navigate = useNavigate();
 
-    const handler=(e)=>{
-        setInputVal(e.target.value);
-        // console.log(e);
+  const location = useLocation();
+
+  const handler = (e) => {
+    setInputVal(e.target.value);
+    // console.log(e);
+  };
+
+  const enterKey = (e) => {
+    if (e.key === "Enter") {
+      // console.log("enter is pressed! and the val: ",inputVal);
+      navigate("/search", { state: { inputVal } });
     }
+  };
 
-    const enterKey=(e)=>{
-        if(e.key==="Enter"){
-            // console.log("enter is pressed! and the val: ",inputVal);
-            navigate('/search',{state:{inputVal}});
-        }
-    }
-
-    // console.log("from state",inputVal);
-    return (
-        <nav className={styles.nav}>
-            <ul>
-                <span>
-                <li>listenIt</li>
-                <li title="Home"><Link to="/"><AiFillHome/></Link></li>
-                </span>
-                <div className={styles.input}>
-                <li><AiOutlineSearch/></li>
-                <input type="text" name="input" id='inputField' placeholder={inputPlaceholder} onChange={handler} onKeyDown={enterKey}/>
-                </div>
-                <span>
-                {/* <li><Link to="/like"><BsSuitHeart/></Link></li> */}
-                <li title="Liked Songs"><Link to="/like"><i className="bi bi-heart"/></Link></li>
-                {/* <li><FaUserCircle/></li> */}
-                <li title="Menu"><BiMenu/></li>
-                </span>
-            </ul>
-        </nav>
-    );
-}
+  return (
+    <nav className={styles.nav}>
+      <ul>
+        <span>
+          <li>listenIt</li>
+          <li title="Home">
+            <Link to="/">
+              <span className={location.pathname === "/" ? styles.active : styles.activeness}>
+                {location.pathname === "/"
+                  ? <RiHome2Fill />
+                  : <RiHome2Line />
+                }
+              </span>
+            </Link>
+          </li>
+        </span>
+        <div className={styles.input}>
+          <li id={styles.searchIcon}>
+            <AiOutlineSearch />
+          </li>
+          <input
+            type="text"
+            name="input"
+            id="inputField"
+            placeholder={inputPlaceholder}
+            onChange={handler}
+            onKeyDown={enterKey}
+          />
+        </div>
+        <span>
+          {/* <li><Link to="/like"><BsSuitHeart/></Link></li> */}
+          <li title="View your Liked Songs">
+            <Link to="/like">
+              <span className={location.pathname === "/like" ? styles.active : styles.activeness}>
+                <i className={location.pathname === "/like" ? "bi bi-heart-fill" : "bi bi-heart"} />
+              </span>
+            </Link>
+          </li>
+          {/* <li><FaUserCircle/></li> */}
+          <li title="Menu">
+            <BiMenu />
+          </li>
+        </span>
+      </ul>
+    </nav>
+  );
+};
 
 export default Header;
--- src/Components/Like/Like.jsx
+++ src/Components/Like/Like.jsx
@@ -1,16 +1,19 @@
 import React,{useState} from 'react';
 import styles from "./Like.module.css";
-const Like = () => {
 
-    const [likeData,setLikeData]=useState([])
-    const noLikeComp=<h3>oop's, No data found</h3>
-    const likeComp=<h3>data is here!</h3>
+const Like = () => {
+    const [likeData,setLikeData]=useState([]);
+    const noLikeComp="Oops!, No data found";
+    const likeComp="Data is here!";
 
     // console.log("from like: ",likeData)
     return (
         <div className={styles.bContainer}>
             <div className={styles.sContainer}>
-                {likeData.length===0?noLikeComp:likeComp}
+                {likeData.length===0
+                    ? <h3 style={{textAlign : "center"}}>{noLikeComp}</h3>
+                    : <h3 style={{textAlign : "center"}}>{likeComp}</h3>
+                }
             </div>
         </div>
     );
